c93eb52d86c7785b524c208baa353aaca3b2178b
Manraaj Nijjar <ManraajNijjar@gmail.com>
2018-09-10 16:07:59 -0400

    Added Client and thus full Decomp functionality

--- calc_client/client.go
+++ calc_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"grpcCourse/calculator/calcpb"
+	"io"
 	"log"
 
 	"google.golang.org/grpc"
@@ -19,7 +20,8 @@ func main() {
 
 	c := calcpb.NewCalculationServiceClient(conn)
 
-	getCalc(c)
+	//getCalc(c)
+	getDecomp(c)
 }
 
 func getCalc(c calcpb.CalculationServiceClient) {
@@ -34,4 +36,26 @@ func getCalc(c calcpb.CalculationServiceClient) {
 		log.Fatalf("Error calling greet: %v", err)
 	}
 	log.Printf("Calculation: %v", res.Result)
+}
+
+func getDecomp(c calcpb.CalculationServiceClient) {
+	req := &calcpb.PrimeNumberDecompRequest{
+		NumberToDecomp: 120,
+	}
+	resStream, err := c.PrimeNumberDecomp(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("error calling stream greet: %v", err)
+	}
+
+	for {
+		factor, err := resStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("error reading stream: %v", err)
+		}
+		log.Printf("factor: %v", factor.GetResult())
+	}
 }
